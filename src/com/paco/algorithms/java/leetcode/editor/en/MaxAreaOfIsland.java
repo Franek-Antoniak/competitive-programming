91a0002cf8c1b24dead7c518b26686ddd213297e
Paco <franekant123@gmail.com>
2022-07-10 00:07:18 +0200

    MaxAreaOfIsland solved, the best solution

--- src/com/paco/algorithms/java/leetcode/editor/en/MaxAreaOfIsland.java
+++ src/com/paco/algorithms/java/leetcode/editor/en/MaxAreaOfIsland.java
@@ -1,61 +1,47 @@
 /**
-Solved by Paco
-Date: 2022-07-08 17:12:37
-Exercise number: 695
-Title: max-area-of-island
-Proramming Language: Java
-*/
-
-//You are given an m x n binary matrix grid. An island is a group of 1's (
-//representing land) connected 4-directionally (horizontal or vertical.) You may assume 
-//all four edges of the grid are surrounded by water. 
-//
-// The area of an island is the number of cells with a value 1 in the island. 
-//
-// Return the maximum area of an island in grid. If there is no island, return 0
-//. 
-//
-// 
-// Example 1: 
-//
-// 
-//Input: grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,
-//0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,
-//0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]
-//]
-//Output: 6
-//Explanation: The answer is not 11, because the island must be connected 4-
-//directionally.
-// 
-//
-// Example 2: 
-//
-// 
-//Input: grid = [[0,0,0,0,0,0,0,0]]
-//Output: 0
-// 
-//
-// 
-// Constraints: 
-//
-// 
-// m == grid.length 
-// n == grid[i].length 
-// 1 <= m, n <= 50 
-// grid[i][j] is either 0 or 1. 
-// 
-// 👍 6441 👎 153
+ * Solved by Paco
+ * Date: 2022-07-08 17:12:37
+ * Exercise number: 695
+ * Title: max-area-of-island
+ * Proramming Language: Java
+ */
 
 
 package com.paco.algorithms.java.leetcode.editor.en;
 
 public class MaxAreaOfIsland {
-//leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public int maxAreaOfIsland(int[][] grid) {
-        
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        private int n1;
+        private int n2;
+
+        public int maxAreaOfIsland(int[][] grid) {
+            int maxV = 0;
+            n1 = grid.length;
+            for (int i = 0; i < grid.length; i++) {
+                n2 = grid[i].length;
+                for (int q = 0; q < grid[i].length; q++) {
+                    if (grid[i][q] == 1)
+                        maxV = Math.max(maxV, getMaxArea(i, q, grid));
+                }
+            }
+            return maxV;
+        }
+
+        private int getMaxArea(int i, int q, int[][] grid) {
+            grid[i][q] = 2;
+            int a = 0, b = 0, c = 0, d = 0;
+            if (i + 1 < n1 && grid[i + 1][q] == 1)
+                a = getMaxArea(i + 1, q, grid);
+            if (i - 1 >= 0 && grid[i - 1][q] == 1)
+                b = getMaxArea(i - 1, q, grid);
+            if (q + 1 < n2 && grid[i][q + 1] == 1)
+                c = getMaxArea(i, q + 1, grid);
+            if (q - 1 >= 0 && grid[i][q - 1] == 1)
+                d = getMaxArea(i, q - 1, grid);
+            return a + b + c + d + 1;
+        }
     }
-}
-//leetcode submit region end(Prohibit modification and deletion)
+    //leetcode submit region end(Prohibit modification and deletion)
 
 }
