17c2190a34a60b206da4db42459a1d730ee0f45b
Paco <franekant123@gmail.com>
2022-07-16 15:39:10 +0200

    PopulatingNextRightPointersInEachNode.java solved, solution faster than 100% of Java submissions.

--- src/com/paco/algorithms/java/leetcode/editor/en/PopulatingNextRightPointersInEachNode.java
+++ src/com/paco/algorithms/java/leetcode/editor/en/PopulatingNextRightPointersInEachNode.java
@@ -1,10 +1,10 @@
 /**
-Solved by Paco
-Date: 2022-07-08 17:13:30
-Exercise number: 116
-Title: populating-next-right-pointers-in-each-node
-Proramming Language: Java
-*/
+ * Solved by Paco
+ * Date: 2022-07-08 17:13:30
+ * Exercise number: 116
+ * Title: populating-next-right-pointers-in-each-node
+ * Proramming Language: Java
+ */
 
 //You are given a perfect binary tree where all leaves are on the same level, 
 //and every parent has two children. The binary tree has the following definition: 
@@ -63,36 +63,49 @@ Proramming Language: Java
 
 package com.paco.algorithms.java.leetcode.editor.en;
 
-public class PopulatingNextRightPointersInEachNode {
-//leetcode submit region begin(Prohibit modification and deletion)
-/*
 // Definition for a Node.
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-    public Node next;
 
-    public Node() {}
-    
-    public Node(int _val) {
-        val = _val;
+public class PopulatingNextRightPointersInEachNode {
+
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
     }
 
-    public Node(int _val, Node _left, Node _right, Node _next) {
-        val = _val;
-        left = _left;
-        right = _right;
-        next = _next;
-    }
-};
-*/
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public Node connect(Node root) {
+            func1(root);
+            return root;
+        }
 
-class Solution {
-//    public Node connect(Node root) {
-//
-//    }
-}
+        private void func1(Node root) {
+            if(root != null && root.right != null) {
+                root.left.next = root.right;
+                if(root.next != null) {
+                    root.right.next = root.next.left;
+                }
+                func1(root.left);
+                func1(root.right);
+            }
+        }
+    }
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
