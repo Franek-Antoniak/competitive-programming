6a7308386ce75bfd1ffad627157bba0c7186931d
Paco <franekant123@gmail.com>
2022-07-19 00:00:41 +0200

    commit

---
+++ src/com/paco/algorithms/java/leetcode/editor/en/FizzBuzz.java
@@ -0,0 +1,20 @@
+/**
+Solved by Paco
+Date: 2022-07-18 23:45:59
+Exercise number: 412
+Title: fizz-buzz
+Proramming Language: Java
+*/
+
+package com.paco.algorithms.java.leetcode.editor.en;
+
+public class FizzBuzz {
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+//    public List<String> fizzBuzz(int n) {
+//
+//    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
