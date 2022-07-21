e33e59a90d776b427d96646a87d4690374758494
Paco <franekant123@gmail.com>
2022-07-22 01:48:05 +0200

    FactorialTrailingZeroes.java solved

--- src/com/paco/algorithms/java/leetcode/editor/en/FactorialTrailingZeroes.java
+++ src/com/paco/algorithms/java/leetcode/editor/en/FactorialTrailingZeroes.java
@@ -12,7 +12,14 @@ public class FactorialTrailingZeroes {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int trailingZeroes(int n) {
-            return 1;
+            if (n < 5)
+                return 0;
+            int counter = 0;
+            while (n > 0) {
+                n /= 5;
+                counter += n;
+            }
+            return counter;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
