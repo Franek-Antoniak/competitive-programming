c9119ca27ab9a44d1265c80af50b6f04c057d709
Paco <franekant123@gmail.com>
2022-07-27 23:45:13 +0200

    Trying to solve DecodeString.java

---
+++ src/com/paco/algorithms/java/leetcode/editor/en/DecodeString.java
@@ -0,0 +1,30 @@
+/**
+ * Solved by Paco
+ * Date: 2022-07-24 19:50:55
+ * Exercise number: 394
+ * Title: decode-string
+ * Programming Language: Java
+ */
+
+package com.paco.algorithms.java.leetcode.editor.en;
+
+public class DecodeString {
+    //leetcode submit region begin(Prohibit modification and deletion)
+//    class Solution {
+//        public String decodeString(String s) {
+//            char[] chars = s.toCharArray();
+//            return getString(chars[0]);
+//
+//        }
+//
+//        public String getString(char[] chars, int index)
+//           if(chars[index] == '[') {
+//               String x = getString(chars, index + 1);
+//               index += x.length() + 1;
+//               return x;
+//           }
+//        }
+//    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
