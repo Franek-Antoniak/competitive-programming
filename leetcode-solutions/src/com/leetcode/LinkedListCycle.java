1a2183e5fb664622c009f3e2e79413b0ed70adce
Franek Antoniak <franekant123@gmail.com>
2022-12-23 01:54:45 +0100

    next solution

---
+++ leetcode-solutions/src/com/leetcode/LinkedListCycle.java
@@ -0,0 +1,28 @@
+package com.leetcode;
+
+public class LinkedListCycle {
+	static class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+			next = null;
+		}
+	}
+
+	class Solution {
+		public boolean hasCycle(ListNode head) {
+			ListNode slow = head;
+			ListNode fast = head;
+			while (fast != null && fast.next != null) {
+				slow = slow.next;
+				fast = fast.next.next;
+				if (slow == fast) {
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+}
