fc5415217a8030fea11602105f73a1db0998f162
Franek Antoniak <franekant123@gmail.com>
2022-12-25 12:55:31 +0100

    solution change

--- leetcode-solutions/src/com/leetcode/MoveZeros.java
+++ leetcode-solutions/src/com/leetcode/MoveZeros.java
@@ -3,10 +3,12 @@ package com.leetcode;
 public class MoveZeros {
 	class Solution {
 		public void moveZeroes(int[] nums) {
-			int j = -1;
+			int j = 0;
 			for (int i = 0; i < nums.length; i++) {
 				if (nums[i] != 0) {
-					nums[i] = (nums[i] + nums[++j]) - (nums[j] = nums[i]);
+					// swap i element with j element
+					nums[i] = (nums[i] + nums[j]) - (nums[j] = nums[i]);
+					j++;
 				}
 			}
 		}
