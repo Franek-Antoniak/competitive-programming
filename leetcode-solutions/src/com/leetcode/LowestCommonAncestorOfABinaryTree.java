29f3865bd44864bc7040a11457540601408ad9e5
Franek Antoniak <franekant123@gmail.com>
2023-01-02 14:15:00 +0200

    next solution

---
+++ leetcode-solutions/src/com/leetcode/LowestCommonAncestorOfABinaryTree.java
@@ -0,0 +1,28 @@
+package com.leetcode;
+
+public class LowestCommonAncestorOfABinaryTree {
+	static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+
+	class Solution {
+		public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+			if (root == null || root == p || root == q) {
+				return root;
+			}
+			TreeNode left = lowestCommonAncestor(root.left, p, q);
+			TreeNode right = lowestCommonAncestor(root.right, p, q);
+			if (left != null && right != null) {
+				return root;
+			}
+			return left != null ? left : right;
+		}
+	}
+}
+
